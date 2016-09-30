d716ff4034b548ff9ccc4277bbf662b379503b65
Chad Coleman <cjc2238@tc.columbia.edu>
2016-09-30 16:42:14 -0400

    Started testing feature generation with sample student data

--- R Scripts/Begin Feature Engineering.R
+++ R Scripts/Begin Feature Engineering.R
@@ -5,23 +5,40 @@ wd_data <- assessments <- read.csv("2014J_data.csv")
 # Set Project working directory
 setwd("~/GitHub/Feature-Engineering-Project")
 
-######################
-### Tidy your data ###
-######################
+########################################
+### Tidy your data using Sample Data ###
+########################################
 
 library(tidyr)
 
-# Figure out how to spread the data
-wd_data_2 <- spread(wd_data, total_click, sum_click)
+# Select student ID to test
+student_101217 <- subset(wd_data, id_student %in% "101217")
+student_552173 <- subset(wd_data, id_student %in% "552173")
+student_655386 <- subset(wd_data, id_student %in% "655386")
 
+# Merge sample student data together
+student_test_data <- rbind(student_655386,student_552173,student_101217)
 
-############################
-### Feature Engineering ###
-############################
+#############################################
+### Feature Engineering Using Sample Data ###
+#############################################
 
 # Create submission time feature
-submission_time <- df_2014j$date.x - df_2014j$date_submitted
+test_data_submission_time <- student_test_data$date.x - student_test_data$date_submitted
+
+# Count number of entrys
+count_of_entrys <- within(student_test_data, { count <- ave(id_student, FUN=function(x) length(unique(x)))})
+names(count_of_entrys) <- c("id_student","number_of_entries")
+
+# Average score of assessment type
+
+# Average number of clicks per activity type
 
 
-# Append submission time to data frame
+# Spread data using tidyr
+
+
+
+
+
 
