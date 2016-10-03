5ad0b3386ab873d6a163a32756dda1dd0c01964b
Chad Coleman <cjc2238@tc.columbia.edu>
2016-10-03 16:36:43 -0400

    Created NN and Ploted output
    Need to remove outliers next and rerun models

--- R Scripts/Normalized_fff_2014j_Linear_Regression_and_ANN.R
+++ R Scripts/Normalized_fff_2014j_Linear_Regression_and_ANN.R
@@ -2,7 +2,7 @@
 df4 <- read.csv("Normalized_fff_2014j_Dataset.csv")
 
 # Select all numeric values
-df5 <- subset(df3, select = -c(df1.final_result, df1.disability, df1.age_band, df1.imd_band, df1.highest_education, df1.region, df1.gender, CMA))
+df5 <- subset(df4, select = -c(df1.final_result, df1.disability, df1.age_band, df1.imd_band, df1.highest_education, df1.region, df1.gender, CMA))
 
 
 # Split data into training and test set at 3/4ths split
@@ -27,15 +27,49 @@ library(neuralnet)
 
 n <- names(train)
 f <- as.formula(paste("TMA ~", paste(n[!n %in% "TMA"], collapse = " + ")))
-nn <- neuralnet(f,data=train,hidden=8, err.fct="sse",linear.output=TRUE, learningrate = 0.3, threshold = 0.5)
+nn <- neuralnet(f,data=train,hidden=c(8,8), err.fct="sse",linear.output=TRUE, learningrate = 0.3, threshold = 0.5)
 
 #############################################
 ## Predicting TMA using the neural network ##
 #############################################
+test1 <- subset(test, select = -c(TMA))
+
+test <- data.frame(test1, test$TMA)
+
+names(test)[names(test) == 'test.TMA'] <- 'TMA'
 
 pr.nn <- compute(nn,test[,1:20])
 
-pr.nn_ <- pr.nn$net.result*(max(data$medv)-min(data$medv))+min(data$medv)
-test.r <- (test_$medv)*(max(data$medv)-min(data$medv))+min(data$medv)
+pr.nn_ <- pr.nn$net.result*(max(df5$TMA)-min(df5$TMA))+min(df5$TMA)
+test.r <- (test$TMA)*(max(df5$TMA)-min(df5$TMA))+min(df5$TMA)
 
-MSE.nn <- sum((test.r - pr.nn_)^2)/nrow(test_)
+MSE.nn <- sum((test.r - pr.nn_)^2)/nrow(test)
+
+##########################
+## Compare the two MSEs ##
+##########################
+
+print(paste(MSE.lm,MSE.nn))
+
+##########################
+## Plot the performance ##
+##########################
+
+par(mfrow=c(1,2))
+
+plot(test$TMA,pr.nn_,col='red',main='Real vs predicted NN',pch=18,cex=0.7)
+abline(0,1,lwd=2)
+legend('bottomright',legend='LM',pch=18,col='blue', bty='n', cex=.95)
+
+plot(test$TMA,pr.lm,col='blue',main='Real vs predicted lm',pch=18, cex=0.7)
+abline(0,1,lwd=2)
+legend('bottomright',legend='LM',pch=18,col='blue', bty='n', cex=.95)
+
+################
+## More Plots ##
+################
+
+plot(test$TMA,pr.nn_,col='red',main='Real vs predicted NN',pch=18,cex=0.7)
+points(test$TMA,pr.lm,col='blue',pch=18,cex=0.7)
+abline(0,1,lwd=2)
+legend('bottomright',legend=c('NN','LM'),pch=18,col=c('red','blue'))
